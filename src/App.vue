264b12e3486797338d165de618d2fbfef4189e5e
novickay <novickaya@sputnikfund.ru>
2022-07-04 12:29:01 +0700

    add account and chainID listeners

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import './assets/styles/App.css';
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+
+/*margin-top: 60px;*/
+</style>
